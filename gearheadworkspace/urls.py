f5fd7b14479cce2dfe2604c30a39e0e3b551f96b
Adam Wonak <adam.wonak@gmail.com>
2014-05-17 11:28:59 -0500

    added new theme. added about and contact pages

--- gearheadworkspace/urls.py
+++ gearheadworkspace/urls.py
@@ -10,5 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'gearheadworkspace.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 	url(r'^$', 'promo.views.index', name='home'),
+	url(r'^about/$', 'promo.views.about', name='about'),
+	url(r'^contact/$', 'promo.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- promo/views.py
+++ promo/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	"""
-	Promotional splash page.
-	"""
-	context = {}
-	return render(request, 'index.html', context)
+    context = {}
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'contact.html', context)
